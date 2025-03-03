8f5f39f478462a99590275ad0bef9935b0966d9f
tarunkishore <tarunkishore@localhost>
2025-03-03 12:17:08 +0530

    TestNGRunner and TMKOC.xml updated

--- BDDTDD/src/main/java/TestRunner/TestNGRunner.java
+++ BDDTDD/src/main/java/TestRunner/TestNGRunner.java
@@ -20,11 +20,5 @@ import io.cucumber.testng.CucumberOptions;
 //		plugin = { "pretty","html:target/cucumber.html" }// tags used in feature file
 		)
 public class TestNGRunner extends AbstractTestNGCucumberTests {
-//	@BeforeClass
-//    public void setUp() {
-//        // Set the tag dynamically using the system property or parameter from XML
-//        String tags = System.getProperty("cucumber.filter.tags","@tag");  // Default to @TMKOC if not provided
-//        System.setProperty("cucumber.filter.tags", tags);  // Set this as a system property
-//    }
 
 }
